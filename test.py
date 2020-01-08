bf8064ebb236a2295ee19d2ea0b97feccb3dca72
Christoph Forster <christoph.forster@gmail.com>
2020-01-08 17:15:34 +0100

    Create test.py

---
+++ test.py
@@ -0,0 +1,8 @@
+from object import da_object
+
+oasch = da_object()
+another_oasch = da_object()
+oasch.firstname = "Anderl"
+another_oasch.firstname = "bärbel"
+oasch.print_name()
+another_oasch.print_name()
